350a7ece246e4c2380fa23aa541245802c9a2db5
Alexey Kuznetsov <axet@me.com>
2017-10-07 21:31:04 +0300

    show popup keyboard when locked

--- app/src/main/java/com/github/axet/torrentclient/activities/PlayerActivity.java
+++ app/src/main/java/com/github/axet/torrentclient/activities/PlayerActivity.java
@@ -122,9 +122,7 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
-                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        MainActivity.showLocked(getWindow());
 
         setContentView(R.layout.activity_player);
 
@@ -354,6 +352,7 @@ public class PlayerActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        MainActivity.updateLocked(getWindow());
     }
 
     @Override
